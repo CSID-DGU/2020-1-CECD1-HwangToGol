26ccb178e0fc015bdb811450e6483d0625572a76
sunneepark <hl2ozs@naver.com>
2020-07-18 14:21:48 +0900

    20/07/18 공 움직임 추가

--- components/index.js
+++ components/index.js
@@ -1,20 +1,16 @@
 const body = document.querySelector('a-scene');
 const $ = (query) => body.querySelector(query);
 
-const shiftDegrees = (value) => (value + 1) % 360;
-
-let degrees = 0;
-
-console.log($('#ball'))
 
 const animate = () => {
-  degrees = shiftDegrees(degrees);
-  const color = `hsl(${degrees}, 100%, 50%)`;
   const variation = Math.sin(Date.now() / 1000); //how many milliseconds pass
-  //const position = `0 ${1.5 + variation} ${-4 + variation} `; //change y-value
 
-  $('#ball').setAttribute('color', color);
-  $('#ball').setAttribute('position',position);
+  let curPosition = $('#ball').getAttribute('position'); //ball's current position
+  
+  curPosition.x -= variation;
+  curPosition.z -= variation;
+
+  $('#ball').setAttribute('position',curPosition);
 
   requestAnimationFrame(animate);
 };
