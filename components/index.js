9ae4638e9fa714e24179d25c46abc7e6a9d44ea3
seonyoung1698 <hwagae2003@gmail.com>
2020-10-13 20:17:15 +0900

    making start scene

--- components/index.js
+++ components/index.js
@@ -1,8 +1,10 @@
+console.log("index.js - 1");
 const body = document.querySelector('a-scene');
 const $ = (query) => body.querySelector(query);
 
 
 const animate = () => {
+
   const variation = Math.sin(Date.now() / 100); //how many milliseconds pass
 
   let curPosition = $('#ball').getAttribute('position'); //ball's current position
