cf75381e4d23f88e5efdc7c3f9e4190a552fe772
seonyoung1698 <hwagae2003@gmail.com>
2020-11-15 21:03:04 +0900

    stage2, 3 edit

--- components/stage1.js
+++ components/stage1.js
@@ -7,6 +7,7 @@ let removeToast;
 var chance = 0;
 
 var txt2;
+var distance;
 
 function makeBall() {
     ball.body.applyImpulse(
@@ -23,10 +24,10 @@ AFRAME.registerComponent('stick', {
         while (ball === undefined) {}
         if(ball.body === undefined) makeBall();
 
-        var distance = ball.body.position.distanceTo(midgatePosition).toFixed(2);
+        distance = ball.body.position.distanceTo(midgatePosition).toFixed(2);
 
         var firsttimer = setTimeout(function () {
-            console.log("chance : ", chance);
+            //console.log("chance : ", chance);
             if (chance == 0) {
                 if (ball.body.position.z < midgatePosition.z && 
                     (ball.body.position.x < 1 && ball.body.position.x > -1)) { //success
@@ -35,22 +36,22 @@ AFRAME.registerComponent('stick', {
 
                 if(ball.body.position.z > midgatePosition.z) { //gate 까지 가지 못했을 때
                     if(ball.body.position.x < 1 && ball.body.position.x > -1){
-                        st1fail("조금 세게 쳐보세요! 마지막 기회가 제공됩니다.공과 게이트 사이의 거리는 "+distance+" m입니다");
+                        st1fail("조금 세게 쳐보세요! 마지막 기회가 제공됩니다.");
                     }
                     if(ball.body.position.x < -1){
-                        st1fail("조금 더 오른쪽으로 세게 쳐보세요! 마지막 기회가 제공됩니다.공과 게이트 사이의 거리는 "+distance+"m입니다");
+                        st1fail("조금 더 오른쪽으로 세게 쳐보세요! 마지막 기회가 제공됩니다.");
                     }
                     if(ball.body.position.x > 1){
-                        st1fail("조금 더 왼쪽으로 세게 쳐보세요! 마지막 기회가 제공됩니다.공과 게이트 사이의 거리는 "+distance+"m입니다");
+                        st1fail("조금 더 왼쪽으로 세게 쳐보세요! 마지막 기회가 제공됩니다.");
                     }
                 }
                 
                 if(ball.body.position.z < midgatePosition.z) {  //gate 넘어감 but 방향 다름
                     if(ball.body.position.x < -1){ // gate 왼쪽으로 
-                        st1fail("조금 더 오른쪽으로 쳐보세요! 마지막 기회가 제공됩니다.공과 게이트 사이의 거리는 "+distance+"m입니다");
+                        st1fail("조금 더 오른쪽으로 쳐보세요! 마지막 기회가 제공됩니다.");
                     }
                     if(ball.body.position.x > 1){  //gate 오른쪽으로
-                        st1fail("조금 더 왼쪽으로 쳐보세요! 마지막 기회가 제공됩니다.공과 게이트 사이의 거리는 "+distance+"m입니다");
+                        st1fail("조금 더 왼쪽으로 쳐보세요! 마지막 기회가 제공됩니다.");
                     }
                 }
                 chance += 1;
@@ -60,7 +61,7 @@ AFRAME.registerComponent('stick', {
 
         if(chance == 1){
             var secondtimer = setTimeout(function() {
-                console.log("chance : ", chance);
+                //console.log("chance : ", chance);
                 if(chance == 1){  //두번재 기회
                     if (ball.body.position.z < midgatePosition.z && 
                         (ball.body.position.x < 1 && ball.body.position.x > -1)) { //success
@@ -69,7 +70,7 @@ AFRAME.registerComponent('stick', {
                         toast('#real_fail');
                         var failAudio = document.getElementById('fail_audio');
                         failAudio.play();
-                        // setTimeout("location.href='gamemain.html'", 3000);
+                        setTimeout("location.href='gamemain.html'", 3000);
                     }
                     chance += 1;
                     clearTimeout(secondtimer);
@@ -80,7 +81,7 @@ AFRAME.registerComponent('stick', {
     }
 });
 function toast(string) {
-    console.log('toast func', string);
+    //console.log('toast func', string);
     const toast = document.getElementById("toast");
     var solutionImg = document.getElementById("solutionImg");
     solutionImg.setAttribute("src",string);
@@ -98,7 +99,7 @@ function toast(string) {
 }
 
 function st1success() {
-    console.log("st1success");
+   // console.log("st1success");
     //toast("축하합니다~! 성공하셨습니다!");
     toast("#success");
     var successAudio = document.getElementById('success_audio');
@@ -107,37 +108,44 @@ function st1success() {
 }
 
 function st1fail(failmsg) {
-    console.log(failmsg);
+    //console.log(failmsg);
     ball.body.position.x = 0;
-    ball.body.position.y = 1.3;
+    ball.body.position.y = -0.25;
     ball.body.position.z = 0;
     
     var failaudio;
+    var distanceArea = document.getElementById('distance');
+
     if(failmsg == '조금 세게 쳐보세요! 마지막 기회가 제공됩니다.'){
         failaudio = document.getElementById('less_power_audio');
-        console.log(failaudio);
+        //console.log(failaudio);
         failaudio.play();
         toast("#fail_power");
+        distanceArea.setAttribute("value",distance+" m");
     }
     if(failmsg == '조금 더 오른쪽으로 세게 쳐보세요! 마지막 기회가 제공됩니다.'){
         failaudio = document.getElementById('right_less_power_audio');
         failaudio.play();
         toast("#fail_right_power");
+        distanceArea.setAttribute("value",distance+" m");
     }
     if(failmsg == '조금 더 왼쪽으로 세게 쳐보세요! 마지막 기회가 제공됩니다.'){
         failaudio = document.getElementById('left_less_power_audio');
         failaudio.play();
         toast("#fail_left_power");
+        distanceArea.setAttribute("value",distance+" m");
     }
     if(failmsg == '조금 더 오른쪽으로 쳐보세요! 마지막 기회가 제공됩니다.'){
         failaudio = document.getElementById('right_enough_power_audio');
         failaudio.play();
         toast("#fail_right");
+        distanceArea.setAttribute("value",distance+" m");
     }
     if(failmsg == '조금 더 왼쪽으로 쳐보세요! 마지막 기회가 제공됩니다.'){
         failaudio = document.getElementById('left_enough_power_audio');
         failaudio.play();
         toast("#fail_left");
+        distanceArea.setAttribute("value",distance+" m");
     }
 
 }
